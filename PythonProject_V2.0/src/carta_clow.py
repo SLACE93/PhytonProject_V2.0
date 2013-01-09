7afe0f5551894fdc1374b9fb0e8d71be2b3e14fb
Ana Arias <acarias@espol.edu.ec>
2013-01-09 00:02:19 -0500

    Creando módulo "carta_clow"

---
+++ PythonProject_V2.0/src/carta_clow.py
@@ -0,0 +1,50 @@
+import sys
+import pygame
+from pygame.locals import *
+import state
+
+class Carta_Agua(state.State):
+    def __init__(self):
+        self.display = pygame.display.get_surface()
+        self.background = pygame.image.load("data/image/paisaje.jpg")
+        self.display.blit(self.background, (0, 0))
+        pygame.display.flip()
+        self.historia = pygame.mixer.Sound("data/sound/audio6.wav")
+        self.historia.play()
+        pygame.time.delay(int(self.historia.get_length()*1000))
+        #self.historia y play
+        #delay
+    
+    def exit(self):
+        self.historia.stop()
+        #self.historia.stop
+    
+    def reason(self):
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+class Carta_Viento(state.State):
+    def __init__(self):
+        self.display = pygame.display.get_surface()
+        self.background = pygame.image.load("data/image/paisaje.jpg")
+        self.display.blit(self.background, (0, 0))
+        pygame.display.flip()
+        self.historia = pygame.mixer.Sound("data/sound/audio7.wav")
+        self.historia.play()
+        pygame.time.delay(int(self.historia.get_length()*1000))
+        #self.historia y play
+        #delay
+    
+    def exit(self):
+        self.historia.stop()
+        #self.historia.stop
+    
+    def reason(self):
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
